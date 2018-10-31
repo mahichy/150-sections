880b425e0bb2603deae0b74263331005647f74ce
mahichy <43366576+mahichy@users.noreply.github.com>
2018-10-31 12:13:07 +0600

    Promise_Services

---
+++ Quickstart pack/assets/js/main.js
@@ -0,0 +1,34 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+
+        $(".staff-list").owlCarousel({
+        	items: 4,
+        	autoplay: false,
+            margin: 30,
+            loop: true,  
+            nav: true,  
+            nevText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>" ] 
+        
+        });
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
