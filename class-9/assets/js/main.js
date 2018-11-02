b7c6c20ac329de76a8c99553b3265ef0e63e8b1d
mahichy <43366576+mahichy@users.noreply.github.com>
2018-11-02 11:48:37 +0600

    owl-carousel

---
+++ class-9/assets/js/main.js
@@ -0,0 +1,36 @@
+(function ($) {
+	       "use strict";
+
+    jQuery(document).ready(function($){
+
+
+        
+
+
+       $(".client-testimonial-carousel").owlCarousel({
+        items: 3,
+        margin: 60,
+        nav: false,
+        dots: true,
+        loop: true,
+
+
+
+
+       });
+            
+        
+     
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
